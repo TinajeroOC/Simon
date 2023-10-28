aa27d5ccb2d44f4da58a66a45cbcb2adc5571d29
Gerami Sadeghi <Gerami.Sadeghi@gmail.com>
2023-10-28 14:15:55 -0700

    Added optimizations and game over menu

--- script.js
+++ script.js
@@ -1,9 +1,67 @@
+//////////////////////////////////////// Declare game state globally ////////////////////////////////////////////////////////////////////////////////
+let pattern = [];        // This array will hold the full current pattern
+let canClick = false;    // Controls when the user can begin entering the pattern
+let patternToGuess = []; // patternToGuess is a copy of the pattern array, which always holds the current pattern
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////// Declare variables globally /////////////////////////////////////////////////////////////////////////////////
+let topLeftTile;
+let topRightTile;
+let bottomLeftTile;
+let bottomRightTile;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////// Event Handlers for each tile ////////////////////////////////////////////////////////////////////////////////
+// The original plan was to add an onclick attribute for each html element, but the tileClicked function could not be seen because the startGame function is inside an event listener from the startGame button
+// Event listeners were removed from the startGame function because after the game restarted, the event listeners would stack and cause unexpected behavior, so we should bind them to event handlers at the start of the startGame function instead
+const handleTopLeftMouseDown = () => {
+    const topLeftTile = document.querySelector('.tile-tl');
+    topLeftTile.style.background = 'white';
+}
 
-// Show welcome screen dynamically after every page load/reload
+const handleTopLeftMouseUp = () => {
+    const topLeftTile = document.querySelector('.tile-tl');
+    topLeftTile.style.background = '';
+    tileClicked(topLeftTile);
+}
+
+const handleTopRightMouseDown = () => {
+    const topRightTile = document.querySelector('.tile-tr');
+    topRightTile.style.background = 'white';
+}
+
+const handleTopRightMouseUp = () => {
+    const topRightTile = document.querySelector('.tile-tr');
+    topRightTile.style.background = '';
+    tileClicked(topRightTile);
+}
+
+const handleBottomLeftMouseDown = () => {
+    const bottomLeftTile = document.querySelector('.tile-bl');
+    bottomLeftTile.style.background = 'white';
+}
+
+const handleBottomLeftMouseUp = () => {
+    const bottomLeftTile = document.querySelector('.tile-bl');
+    bottomLeftTile.style.background = '';
+    tileClicked(bottomLeftTile);
+}
+
+const handleBottomRightMouseDown = () => {
+    const bottomRightTile = document.querySelector('.tile-br');
+    bottomRightTile.style.background = 'white';
+}
+
+const handleBottomRightMouseUp = () => {
+    const bottomRightTile = document.querySelector('.tile-br');
+    bottomRightTile.style.background = '';
+    tileClicked(bottomRightTile);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////// Event listener to show the welcome screen dynamically after every page load/reload //////////////////////////
 document.addEventListener('DOMContentLoaded', () => {
     displayMainMenu();
 });
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////// Function that displays how to play //////////////////////////////////////////////////////////////////////////
 const displayMainMenu = () => {
     const gameControlLayout = document.querySelector('.game-control-layout');
     gameControlLayout.innerHTML = ''; // Remove any existing html
@@ -30,9 +88,8 @@ const displayMainMenu = () => {
     gameControlLayout.appendChild(startBtn);
     gameControlLayout.appendChild(howToPlayBtn);
 }
-
-
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////// Function that displays how to play //////////////////////////////////////////////////////////////////////////
 const displayHowToPlay = () => {
     const gameControlLayout = document.querySelector(".game-control-layout");
     gameControlLayout.innerHTML = ''; // Remove any existing html
@@ -58,125 +115,157 @@ const displayHowToPlay = () => {
     gameControlLayout.appendChild(instructions);
     gameControlLayout.appendChild(backToMenu);
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////// This function returns a random tile for the next item in the pattern ///////////////////////////////////////
+const getRandomTile = () => {
+    // Create a group of all tiles for the function to choose from
+    const tiles = [
+        topLeftTile,
+        topRightTile,
+        bottomLeftTile,
+        bottomRightTile,
+    ];
+    // Gets a random value between 0 and 3, then rounds it down to the nearest whole number
+    // The result is a random index corresponding to one of the tiles in the array
+    return tiles[parseInt(Math.random() * tiles.length)]
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////// This function records the tiles the user clicks /////////////////////////////////////////////////////////////
+const tileClicked = selectedTile => {
+    if (!canClick) return;
+    // The next tile we are expecting to see in the users pattern is the next tile in the patternToGuess array
+    // Each time the user clicks a correct tile, the shift() function puts the next tile in the pattern inside 'expectedTile'
+    // shift() will always remove the first element in the patternToGuess array, which corresponds with the next tile in the pattern that should be clicked
+    const expectedTile = patternToGuess.shift();
+    // Check if the correct tile was selected
+    if (expectedTile === selectedTile) {
+        // Check if the tile that was clicked was the last tile in the current pattern
+        if (patternToGuess.length === 0) {
+            // Add a new random tile to the pattern
+            pattern.push(getRandomTile());
+            // Put the the new pattern with the new random tile inside the patternToGuess array
+            // The reason we need patternToGuess is because shift() devours the current element, so each time the pattern is used up, we need to re-copy it from the pattern array
+            patternToGuess = [...pattern];
+            setTimeout(() => {
+                // Start outputting the next pattern after 500 ms
+                startFlashing();
+            }, 500)
+        }
+    }
+    else { // If the wrong tile was selected, end the game
+        endGame();
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////// This function controls the flashing for each tile ///////////////////////////////////////////////////////////
+const flash = tile => {
+    return new Promise((resolve, reject) => {
+        // Activates the current tile in the pattern
+        tile.className += ' active';
+        // Set a timer for how long the active tile flashes before changing the active tile
+        setTimeout(() => {
+            tile.className = tile.className.replace(
+                ' active',
+                ''
+            );
+            // In case the pattern has the same tile back to back, it will be 2 short flashes instead of one long flash
+            setTimeout(() => {
+                resolve();
+            }, 200);
+        }, 700);
+    });
+}
 
-
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////// This function controls the pattern flashes /////////////////////////////////////////////////////////////////
+const startFlashing = async () => {
+    // The user cannot click any tiles while the pattern is being displayed 
+    canClick = false;
+    for (const tile of pattern) {
+        // await promise so every tile doesn't flash at the same time
+        await flash(tile);
+    }
+    // The user can now click tiles since the pattern has finished flashing
+    canClick = true;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////// Main function to run game ////////////////////////////////////////////////////////////////////////////
 const startGame = () => {
     // Hide main menu
     let gameControlLayout = document.querySelector('.game-control-layout');
     gameControlLayout.style.display = 'none';
 
     // Get each tile
+    topLeftTile = document.querySelector('.tile-tl');
+    topRightTile = document.querySelector('.tile-tr');
+    bottomLeftTile = document.querySelector('.tile-bl');
+    bottomRightTile = document.querySelector('.tile-br');
+
+    // Bind event listeners to event handlers
+    topLeftTile.addEventListener('mousedown', handleTopLeftMouseDown);
+    topLeftTile.addEventListener('mouseup', handleTopLeftMouseUp);
+    topRightTile.addEventListener('mousedown', handleTopRightMouseDown);
+    topRightTile.addEventListener('mouseup', handleTopRightMouseUp);
+    bottomLeftTile.addEventListener('mousedown', handleBottomLeftMouseDown);
+    bottomLeftTile.addEventListener('mouseup', handleBottomLeftMouseUp);
+    bottomRightTile.addEventListener('mousedown', handleBottomRightMouseDown);
+    bottomRightTile.addEventListener('mouseup', handleBottomRightMouseUp);
+
+    // Initialize the pattern with the first random tile
+    pattern = [getRandomTile()];
+
+    // This array is a copy of the pattern
+    // The user will keep guessing the next tile, and each time the first element will be removed so the next tile can be checked
+    // After the user has guessed all correct tiles and this array is empty, it will copy the pattern, including the new tile at the end, inside the tileClicked function
+    // First iteration it just copies the only element in the pattern array, which is the result from getRandomTile
+    patternToGuess = [...pattern];
+
+    startFlashing();
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////// Function to reset game after loss //////////////////////////////////////////////////////////////////////////
+
+
+const endGame = () => {
+    // Show game over menu
+    const gameControlLayout = document.querySelector('.game-control-layout');
+    gameControlLayout.innerHTML = ''; // Clear all existing HTML
+    gameControlLayout.style.display = 'flex';
+    gameControlLayout.style.justifyContent = 'center';
+
+    const h3 = document.createElement('h3');
+    h3.className = 'title';
+    h3.innerText = 'You Lost!';
+
+    const btn = document.createElement('button');
+    btn.className = 'button';
+    btn.innerText = 'Try Again'
+
+    btn.addEventListener('click', startGame);
+
+    gameControlLayout.appendChild(h3);
+    gameControlLayout.appendChild(btn);
+
+    // Get tiles
     const topLeftTile = document.querySelector('.tile-tl');
     const topRightTile = document.querySelector('.tile-tr');
     const bottomLeftTile = document.querySelector('.tile-bl');
     const bottomRightTile = document.querySelector('.tile-br');
 
-    // Add event listeners for each tile
-    // The original plan was to add an onclick attribute for each html element, but the tileClicked function could not be seen because the startGame function is inside an event listener from the startGame button
-    topLeftTile.addEventListener('click', () => {
-        tileClicked(topLeftTile);
-    });
-    
-    topRightTile.addEventListener('click', () => {
-        tileClicked(topRightTile);
-    });
-    
-    bottomLeftTile.addEventListener('click', () => {
-        tileClicked(bottomLeftTile);
-    });
-    
-    bottomRightTile.addEventListener('click', () => {
-        tileClicked(bottomRightTile);
-    });
-    
-
-
-    // This function returns a random tile for the next item in the pattern
-    const getRandomTile = () => {
-        // Create a group of all tiles for the function to choose from
-        const tiles = [
-            topLeftTile,
-            topRightTile,
-            bottomLeftTile,
-            bottomRightTile,
-        ];
-        // Gets a random value between 0 and 3, then rounds it down to the nearest whole number
-        // The result is a random index corresponding to one of the tiles in the array
-        return tiles[parseInt(Math.random() * tiles.length)]
-    }
-
-    // This array will hold the full current pattern
-    const pattern = [
-        getRandomTile()
-    ];
-    // This array is a copy of the pattern
-    // The user will keep guessing the next tile, and each time the first element will be removed so the next tile can be checked
-    // After the user has guessed all correct tiles and this array is empty, it will copy the pattern, including the new tile at the end, inside the tileClicked function
-    // For now it just copies the only element in the pattern array, which is the result from getRandomTile
-    let patternToGuess = [...pattern];
-
-    // This function controls the flashing for each tile
-    const flash = tile => {
-        return new Promise((resolve, reject) => {
-            // Activates the current tile in the pattern
-            tile.className += ' active';
-            // Set a timer for how long the active tile flashes before changing the active tile
-            setTimeout(() => {
-                tile.className = tile.className.replace(
-                    ' active',
-                    ''
-                );
-                // In case the pattern has the same tile back to back, it will be 2 short flashes instead of one long flash
-                setTimeout(() => {
-                    resolve();
-                }, 200);
-            }, 900);
-        });
-    }
-
-    // The user cannot click any tiles yet
-    let canClick = false;
-
-    // This function records the tiles the user clicks
-    const tileClicked = selectedTile => {
-        if (!canClick) return;
-        // The next tile we are expecting to see in the users pattern is the next tile in the patternToGuess array
-        // Each time the user clicks a correct tile, the shift() function puts the next tile in the pattern inside 'expectedTile'
-        // shift() will always remove the first element in the patternToGuess array, which corresponds with the next tile in the pattern that should be clicked
-        const expectedTile = patternToGuess.shift();
-        // Check if the correct tile was selected
-        if (expectedTile === selectedTile) {
-            // Check if the tile that was clicked was the last tile in the current pattern
-            if (patternToGuess.length === 0) {
-                // Add a new random tile to the pattern
-                pattern.push(getRandomTile());
-                // Put the the new pattern with the new random tile inside the patternToGuess array
-                // patternToGuess is a copy of the pattern array, which always holds the current pattern
-                // The reason we need patternToGuess is because shift() devours the current element, so each time the pattern is used up, we need to re-copy it from the pattern array
-                patternToGuess = [...pattern];
-                // Once the new pattern is ready, start outputting it
-                startFlashing();
-            }
-        }
-        else { // If the wrong tile was selected, end the game
-            alert('You lose. You have brought shame upon your family :(');
-        }
-    }
-
-    const startFlashing = async () => {
-        // The user cannot click any tiles while the pattern is being displayed 
-        canClick = false;
-        for (const tile of pattern) {
-            // await promise so every tile doesn't flash at the same time
-            await flash(tile);
-        }
-        // The user can now click tiles since the pattern has finished flashing
-        canClick = true;
-    }
-
-    startFlashing();
+    // Remove event listeners so they don't stack
+    topLeftTile.removeEventListener('mousedown', handleTopLeftMouseDown);
+    topLeftTile.removeEventListener('mouseup', handleTopLeftMouseUp);
+    topRightTile.removeEventListener('mousedown', handleTopRightMouseDown);
+    topRightTile.removeEventListener('mouseup', handleTopRightMouseUp);
+    bottomLeftTile.removeEventListener('mousedown', handleBottomLeftMouseUp);
+    bottomLeftTile.removeEventListener('mouseup', handleBottomLeftMouseUp);
+    bottomRightTile.removeEventListener('mousedown', handleBottomRightMouseDown);
+    bottomRightTile.removeEventListener('mouseup', handleBottomRightMouseUp);
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 
 //////////////////////////// Ways we can improve the code
 
